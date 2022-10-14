1fc31aca9ed6b048a4f4bcfca68deabe45e1c235
Abizar Valentino Fachri <203040086@main.unpas.ac.id>
2022-10-14 13:45:36 +0700

    Mempelajari MVC

---
+++ phpmvc2/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_B_203040086/phpmvc2/public');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
---
+++ phpmvc2/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL ?>/img/bubu.jpg" alt="Abizar Valentino Fachri" width="200" class="rounded shadow">
+  <p>Halo, Nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> Tahun, Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
